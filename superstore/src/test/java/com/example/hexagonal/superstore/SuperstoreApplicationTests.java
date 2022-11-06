a4b29524ccd10f313796a5859dfc5db94414f955
rph-goes <rph.goes@gmail.com>
2022-11-06 10:26:39 -0300

    modulo de teste

---
+++ superstore/src/test/java/com/example/hexagonal/superstore/SuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal.superstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
