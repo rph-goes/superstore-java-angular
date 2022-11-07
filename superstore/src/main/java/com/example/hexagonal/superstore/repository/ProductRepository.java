6a8199aeff77e9de4a1fcb0447b87454fd01ad8f
rph-goes <rph.goes@gmail.com>
2022-11-07 17:43:05 -0300

    adicionando função de busca

--- superstore/src/main/java/com/example/hexagonal/superstore/repository/ProductRepository.java
+++ superstore/src/main/java/com/example/hexagonal/superstore/repository/ProductRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.hexagonal.superstore.model.Product;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
+  void deleteProductById(Long id);
 
+  Optional<Product> findProductById(Long id);
 }
